36eba6269f2ee820f3281b7a53f10d44c7ca3daa
Sai Chandan <saichandan@Sais-MacBook-Air.local>
2023-02-08 16:27:46 +0530

    Callback functions examples

---
+++ my-simple-plugin/my-simple-plugin.php
@@ -0,0 +1,16 @@
+<?php /**
+ * Plugin Name: My Simple Plugin
+ * Plugin URI: ''
+ * Author: Chandan
+ * Author URI: ''
+ * Version: 0.0.1
+ * Description: A new Plugin
+ * Text Domain: 'my-simple-plugin'
+ * *
+ * 
+ */
+
+ require_once dirname(__FILE__).'/include/callbacks.php';
+ 
+?>
+
