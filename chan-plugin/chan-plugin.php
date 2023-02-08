df92f56f65afb1e9d1f04a55daf2d405de432dd1
Sai Chandan <saichandan@Sais-MacBook-Air.local>
2023-02-08 09:34:08 +0530

    resolving the code

--- chan-plugin/chan-plugin.php
+++ chan-plugin/chan-plugin.php
@@ -1,25 +1,35 @@
 <?php /**
- * Plugin Name: chan-plugin
+ * Plugin Name: My Simple Plugin
  * Plugin URI: ''
  * Author: Chandan
  * Author URI: ''
  * Version: 2.3.5
- * Description: 'A new Plugin'
- * Text Domain: 'xyz'
+ * Description: A new Plugin
+ * Text Domain: 'My-Simple-Plugin'
  * *
  * 
  */
 
- //Function to handle a shortcode
+ 
+ /**
+  * Handles a shortcode on posts
+  *
+  * @since 8.1.9
+  *
+  * @return HTML snippet which includes shortcode default attributes if user haven't provided any attributes.
+  */
 
-function handle_shortcode( $attr, $content='' ){
-    global $post;       //global post variable
+ function handle_shortcode( $attr, $content='' ){
+    //global post variable
+    global $post;       
 
-    $attr = shortcode_atts( array(      //default attribute values
-        'title' => 'Default title',
-        'color' => 'red' 
+    //default attribute values
+    $attr = shortcode_atts( array(      
+        'title' => __('Default title', 'my-text-domain'),
+        'color' => __('red', 'my-text-domain'), 
     ), $attr);
-    ob_start();     //using ouput buffer
+    //using ouput buffer
+    ob_start();     
     ?>
 
     <div style="color:<?php echo $attr['color']?>">
@@ -27,24 +37,49 @@ function handle_shortcode( $attr, $content='' ){
     <?php echo $post->ID;?>
     </div>
     <?php
-    return ob_get_clean(); //output buffer ends
+    //output buffer ends
+    return ob_get_clean(); 
 }
 // Hooking function to test shortcode hook
 add_shortcode("test-shortcode",'handle_shortcode');
 
-function add_content_bottom($content){
-    return $content." My content at bottom"; //modifying the original content
-}
-add_filter('the_content', 'add_content_bottom'); //the_content hook to add custom content
 
-function inject_ad( $posts) { //A function to add a ad post
+
+ /**
+  * Modifies the original content
+  *
+  * @since 8.1.9
+  *
+  * @return shortcode content with modifications
+  */
+
+function add_content_bottom($content){
+    
+    //modifying the original content
+    return $content." My content at bottom"; 
+}
+//the_content hook to add custom content
+add_filter('the_content', 'add_content_bottom'); 
+
+
+/**
+  * Add a new post 'Advertisement' to list of posts'
+  *
+  * @since 8.1.9
+  *
+  * @return array list with new post addition
+  */
+
+function inject_ad( $posts) {
     if ( is_home() && is_main_query() ){
         $page = get_page_by_title('Advertisement', OBJECT, 'post');
         array_splice($posts, 1,0,array($page));
     }
+    
     return $posts;
 }
-
-add_filter('the_posts', 'inject_ad'); //filter hook
- ?>
+//filter hook
+add_filter('the_posts', 'inject_ad'); 
+ 
+?>
 
